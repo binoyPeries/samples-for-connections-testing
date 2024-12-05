79e22f69593391c740986c75f76b564c9cfaf926
Hansi Karunarathna <65526190+hanzjk@users.noreply.github.com>
2024-12-05 21:01:41 +0530

    Update main.go

--- greeting-service-go/main.go
+++ greeting-service-go/main.go
@@ -21,7 +21,6 @@ package main
 import (
 	"context"
 	"errors"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -29,7 +28,6 @@ import (
 	"syscall"
 	"time"
         "io"
-        "golang.org/x/oauth2/clientcredentials"
 )
 
 func main() {
