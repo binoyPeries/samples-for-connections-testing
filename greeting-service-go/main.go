48aced675c9e8f8ab31790ac47881d35ee1ff190
Hansi Karunarathna <65526190+hanzjk@users.noreply.github.com>
2025-08-15 17:08:05 +0530

    Update main.go

--- greeting-service-go/main.go
+++ greeting-service-go/main.go
@@ -68,11 +68,11 @@ func main() {
 
 func greet(w http.ResponseWriter, r *http.Request) {
 	// Read environment variables
-	serviceURL := os.Getenv("CHOREO_TESTCON5_SERVICEURL")+"/greeting"
-	tokenURL := os.Getenv("CHOREO_TESTCON5_TOKENURL")
-	clientSecret := os.Getenv("CHOREO_TESTCON5_CONSUMERSECRET")
-	clientID := os.Getenv("CHOREO_TESTCON5_CONSUMERKEY")
-	apiKey := os.Getenv("CHOREO_TESTCON5_CHOREOAPIKEY")
+	serviceURL := os.Getenv("CHOREO_TESTCON7_SERVICEURL")+"/greeting"
+	tokenURL := os.Getenv("CHOREO_TESTCON7_TOKENURL")
+	clientSecret := os.Getenv("CHOREO_TESTCON7_CONSUMERSECRET")
+	clientID := os.Getenv("CHOREO_TESTCON7_CONSUMERKEY")
+	apiKey := os.Getenv("CHOREO_TESTCON7_CHOREOAPIKEY")
 	// Log the client ID for debugging
 	fmt.Printf("Client ID: %s\n", clientID)
 	fmt.Printf("serviceURL: %s\n", serviceURL)
